c97f7cacd748ffe0de310e644cf09d5f0c99bab1
fayd <faisal.sayed502@gmail.com>
2020-10-11 14:06:50 +0000

    Added prediction text

--- scan.js
+++ scan.js
@@ -1,10 +1,11 @@
 const URL = "https://teachablemachine.withgoogle.com/models/IngU9ZhbD/";
-
 let model, webcam, labelContainer, maxPredictions;
+const text = document.querySelector(".text-display");
 
 async function init() {
 	const modelURL = URL + "model.json";
 	const metadataURL = URL + "metadata.json";
+
 
 	model = await tmImage.load(modelURL, metadataURL);
 	maxPredictions = model.getTotalClasses();
@@ -17,22 +18,27 @@ async function init() {
 
 	document.getElementById("webcam-container").appendChild(webcam.canvas);
 	labelContainer = document.getElementById("label-container");
-	for (let i = 0; i < maxPredictions; i++) { // and class labels
+	for (let i = 0; i < maxPredictions; i++) {
 			labelContainer.appendChild(document.createElement("div"));
 	}
 }
 
 async function loop() {
-		webcam.update();
-		await predict();
-		window.requestAnimationFrame(loop);
+	webcam.update();
+	await predict();
+	window.requestAnimationFrame(loop);
 }
 
 async function predict() {
-		const prediction = await model.predict(webcam.canvas);
-		for (let i = 0; i < maxPredictions; i++) {
-				const classPrediction =
-						prediction[i].className + ": " + prediction[i].probability.toFixed(2);
-				labelContainer.childNodes[i].innerHTML = classPrediction;
-		}
+	const prediction = await model.predict(webcam.canvas);
+	for (let i = 0; i < maxPredictions; i++) {
+			const classPrediction =
+					prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+			labelContainer.childNodes[i].innerHTML = classPrediction;
+  }
+	if (prediction[1].probability > 0.5) {
+		text.innerHTML = "This item is not recyclable.";
+	} else if (prediction[1].probability < 0.5) {
+		text.innerHTML = "This item seems to be recyclable.";
+	}
 }
